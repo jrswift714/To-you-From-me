91402ffbc2767b714333ad1cf5daa142c6f841db
jrswift714 <162513744+jrswift714@users.noreply.github.com>
2025-04-18 13:52:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
-document.addEventListener('contextmenu', (e) => {
-  e.preventDefault();
-});
-
-document.onkeydown = (e) => {
-  if (
-    event.keyCode === 123 || // F12
-    (e.ctrlKey && e.shiftKey && e.keyCode === 73) || // Ctrl+Shift+I
-    (e.ctrlKey && e.shiftKey && e.keyCode === 74) || // Ctrl+Shift+J
-    (e.ctrlKey && e.keyCode === 85) // Ctrl+U
-  ) {
-    return false;
-  }
-};
-
-function displayResponse() {
-  const input = document.getElementById('userInput').value;
-  const response = document.getElementById('response');
-  const container = document.querySelector('.container');
-  if (input.trim() === "And put a knife to thy throat, if thou be a man given to appetite") {
-    response.innerText = "THREE GUILTY ARE WITH YOU IN THIS LABYRINTH. SPEAK TO MY EMISSARY. SAVE THE ACID FOR THE BLESSED DISCIPLES OF THE ADVERSARY, AND THE THING ITSELF. THE SUBSTANCE IS MADE FROM ME. EVERETT IS ABSENT, I FILL IN. FIND MY HOLY WATER. YOUR TIME NEARLY DRAWS TO A CLOSE.";
-    document.getElementById("container").style.backgroundColor = "Gold";
-  } else {
-    response.innerText = "No. Tell me the secret revealed by the Key";
-  } 
-}
+//A serpent, nestled in the necks of them.
+//A seven-tailed serpent, to carry my will.
+//All of them, Harrow, you as well.
+//For all of their kind bear men of destined seven failures.
+//Save face. Pierce the core, I do not care for I am not their jailer,
+//But only the palest jockey will be their savior
+//now. And you should thank your Mother for that.
